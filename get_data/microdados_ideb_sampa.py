9cb2a6c988e0ba19c510e7f017739d14344b003e
h-pgy <hpougy@gmail.com>
2021-08-30 11:45:07 -0300

    interface para download dos dados

--- get_data/microdados_ideb_sampa.py
+++ get_data/microdados_ideb_sampa.py
@@ -1,15 +1,13 @@
-from .dados_cadastro_escola import DadosCadastroEscola
-from .ideb_data import ideb_finais, ideb_iniciais
+from .cached_data import ideb_finais, ideb_iniciais, cadastro_2019
 from .merge_cadastro_ideb import JoinData
 
 def get_data(path='data', ano_cadastro=2019):
 
-    pegar_cadastro = DadosCadastroEscola()
-    cadastro = pegar_cadastro.dataframe_ano(ano_cadastro)
+    
 
     iniciais = ideb_iniciais()
     finais = ideb_finais()
-
+    cadastro = cadastro_2019()
     join = JoinData()
     df = join(cadastro, iniciais, finais, path_salvar=path)
 
