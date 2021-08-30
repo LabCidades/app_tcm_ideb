63a4e6f2b894cf51b366c86959e180e0e7ea5c34
h-pgy <hpougy@gmail.com>
2021-08-30 14:36:28 -0300

    file para baixar shape distritos

---
+++ get_data/distritos_shp.py
@@ -0,0 +1,40 @@
+import requests
+import os
+
+class DownloadShapeDists:
+    
+    url_distritos = ('http://download.geosampa.prefeitura.sp.gov.br/PaginasPublicas/'
+                'downloadArquivo.aspx?orig=DownloadCamadas&'
+                'arq=01_Limites%20Administrativos%5C%5CDistrito%5C%5CShapefile%5C%5CSIRGAS_SHP_distrito&arqTipo=Shapefile')
+    
+    path_dados = 'data/geo_data'
+    
+    def download(self):
+        
+        with requests.get(self.url_distritos) as r:
+            shape_zip = r.content
+        return shape_zip
+    
+    def solve_data_dir(self, data_dir=None):
+        
+        if data_dir is None:
+            data_dir = self.path_dados
+            
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+        
+        return data_dir
+    
+    def unzip(self, shape_zip, path_dados=None):
+                
+        zip_file = ZipFile(BytesIO(shape_zip))
+        path_dados = self.solve_data_dir(path_dados)
+        
+        zip_file.extractall(path_dados)
+        
+    def __call__(self):
+        
+        shape_zip = self.download()
+        
+        self.unzip(shape_zip)
+        print("Shape distritos baixado e salvado com sucesso")
